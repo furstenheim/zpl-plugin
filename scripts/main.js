a6db4eeb230a028d807104145802bd95da18dfaf
Gabi <furstenheim@gmail.com>
2020-03-14 17:33:53 +0100

    Clean up zpl output

--- scripts/main.js
+++ scripts/main.js
@@ -92,9 +92,87 @@ for (let i = 0; i < mainDocument.length; i++){
     commands.push(currentCode)
     status = CODE
   }
-
 }
-console.log(commands)
+// console.log(commands)
+
+const cleanedCommands = commands.map(function (c) {
+  return {
+    code: c.code,
+    definition: c.definition.trim(),
+    format: c.format.map(f => f.replace('Format', '').trim()),
+    description: cleanDescription(c.description),
+    parameters: cleanParameters(c.parameters)
+  }
+})
+
+fs.writeFileSync('zpl.json', JSON.stringify(cleanedCommands, null, 2))
+console.log(cleanedCommands)
+
+
+function cleanDescription (d) {
+  const padding = d[0].indexOf('D')
+  const withoutPadding = d.map(line => line.substring(padding))
+  withoutPadding[0].replace('Description ', '')
+  return withoutPadding
+}
+
+/**
+ *
+ * @param {Array} p
+ */
+function cleanParameters (p) {
+  let leftColumnIndex = Infinity
+  let rightColumnIndex = Infinity
+  const result = []
+  for (let i = 0; i < p.length; i++){
+    let line = p[i]
+    const possibleParameter = line.match(/^(\s*)(\w|[<>#])+ = /)
+    if (possibleParameter) {
+      if (possibleParameter[1].length < rightColumnIndex) {
+        console.log(rightColumnIndex, possibleParameter[1].length)
+        leftColumnIndex = possibleParameter[1].length
+        const subText = line.substring(leftColumnIndex)
+        const match = subText.match(/(.*)(\s\s+)(.*)/)
+        result.push(subText)
+
+        if (match) {
+          // Example         Note • This parameter is ignored on the R110Xi HF printer because
+          const adjust = match[3].includes('Note') ? '        '.length : 0
+          rightColumnIndex = leftColumnIndex + match[1].length + match[2].length - adjust
+          console.log('~~~~4', line, match[3])
+          continue
+        }
+      //  v = reverse the data order Reverses the data order
+        const startIndex = mulIndexOf(subText, ['Accepted Values', 'Reverses the data order'])
+        if (startIndex === -1) {
+          console.error(line)
+          throw new Error('Unexpected line')
+        }
+        rightColumnIndex = leftColumnIndex + startIndex
+
+        continue
+      }
+    }
+
+    if (!line.substr(0, leftColumnIndex).match(/\s*/)) {
+      console.error(line, rightColumnIndex)
+      throw new Error('Line does not match')
+    }
+    result.push(line.substring(leftColumnIndex))
+  }
+  return result
+}
+
+function mulIndexOf (string, values) {
+  for (const value of values) {
+    const index = string.indexOf(value)
+    if (index !== -1) {
+      return index
+    }
+  }
+  return -1
+}
+/*console.log(commands)*/
 function toIndex (i) {
   return i + 1283
 }
