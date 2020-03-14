e669bdd12891960ce4892f84b0206faa8fab97f0
Gabi <furstenheim@gmail.com>
2020-03-14 12:40:55 +0100

    Access text on line

--- src/main/java/PopupDialogAction.java
+++ src/main/java/PopupDialogAction.java
@@ -1,16 +1,33 @@
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.LogicalPosition;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.TextRange;
 import org.jetbrains.annotations.NotNull;
 
 public class PopupDialogAction extends AnAction {
+    @Override
+    public void update(@NotNull AnActionEvent e) {
+        super.update(e);
+    }
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
+        final Project project = e.getProject();
+        final Editor editor = e.getData(CommonDataKeys.EDITOR);
         System.out.println("Aaaa");
-    }
-
-    @Override
-    public boolean isDumbAware() {
-        return false;
+        if (editor == null) {
+            return;
+        }
+        LogicalPosition position = editor.getCaretModel().getPrimaryCaret().getLogicalPosition();
+        int offset = editor.getCaretModel().getPrimaryCaret().getOffset();
+        LogicalPosition startOfLine = new LogicalPosition(position.line, 0);
+        Document document = editor.getDocument();
+        int lineStartOffset = document.getLineStartOffset(position.line);
+        document.getText(new TextRange(lineStartOffset, offset));
     }
 }
+
